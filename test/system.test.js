ec1178a5084e42d415c86ca8af53de50b2a123dd
Alexander Kingston Bailey <alexkingstonbailey@gmail.com>
2021-04-25 21:12:19 +1200

    [NZPMC-001] created json files from yaml, added nodemon and esm (#1)

--- test/system.test.js
+++ test/system.test.js
@@ -1,12 +1,12 @@
-const assert = require('assert');
-const {request} = require('gaxios');
+import assert from 'assert';
+import { request } from 'gaxios';
 
 const port = process.env.PORT || '8080';
 const url = process.env.SERVICE_URL || `http://localhost:${port}`;
 
 describe('Hello World', () => {
   it('can respond to an HTTP request', async () => {
-    console.log(`    - Requesting GET ${url}/...`)
+    console.log(`    - Requesting GET ${url}/...`);
     const res = await request({
       url: url + '/',
       timeout: 5000,
@@ -14,7 +14,9 @@ describe('Hello World', () => {
 
     assert.equal(res.status, '200');
     assert.ok(
-      res.data.includes('Congratulations, you successfully deployed a container image to Cloud Run'),
+      res.data.includes(
+        'Congratulations, you successfully deployed a container image to Cloud Run'
+      )
     );
   });
 });
