a5ad44801e9710eadaf187b8cc0bca0cc5590106
Will-ZYS <54428242+Will-ZYS@users.noreply.github.com>
2021-05-01 23:34:56 +1200

    Nzpmc 001/setup frontend file structure (#4)
    * extrcted eslint config from package.json
    
    * Deleted Vue Helloworld templates
    
    Co-authored-by: Lance Delos Reyes <lanxdrey19@users.noreply.github.com>
    
    * clean ups
    
    * Structural change
    
    * added prettierignore
    
    * added eslint rules for github lint action
    
    * added github action cache for node
    
    * changed github caching settings
    
    Co-authored-by: Lance Delos Reyes <lanxdrey19@users.noreply.github.com>

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -1,20 +1,20 @@
-import Vue from 'vue';
-import Vuetify from 'vuetify/lib/framework';
+import Vue from 'vue'
+import Vuetify from 'vuetify/lib/framework'
 
-Vue.use(Vuetify);
+Vue.use(Vuetify)
 
 export default new Vuetify({
-  theme: {
-    themes: {
-      light: {
-        primary: '#03a9f4',
-        secondary: '#009688',
-        accent: '#3f51b5',
-        error: '#f44336',
-        warning: '#ffeb3b',
-        info: '#00bcd4',
-        success: '#4caf50',
-      },
+    theme: {
+        themes: {
+            light: {
+                primary: '#03a9f4',
+                secondary: '#009688',
+                accent: '#3f51b5',
+                error: '#f44336',
+                warning: '#ffeb3b',
+                info: '#00bcd4',
+                success: '#4caf50',
+            },
+        },
     },
-  },  
-});
+})
