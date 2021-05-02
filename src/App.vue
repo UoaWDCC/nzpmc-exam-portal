b88b3307d3417deddcce79efd3d4328220693f4e
matthew <matthewaptaylor@gmail.com>
2021-05-02 22:20:51 +1200

    Added background and font

--- src/App.vue
+++ src/App.vue
@@ -1,34 +1,30 @@
 <template>
     <div id="app">
-        <div id="nav">
-            <router-link to="/">Login</router-link> |
-            <router-link to="/welcome">Welcome</router-link> |
-            <router-link to="/exam">Exam</router-link> |
-            <router-link to="/finished">Finished</router-link>
-        </div>
+        <router-link to="/">Login</router-link> |
+        <router-link to="/welcome">Welcome</router-link> |
+        <router-link to="/exam">Exam</router-link> |
+        <router-link to="/finished">Finished</router-link>
         <router-view />
     </div>
 </template>
 
 <style lang="scss">
+@import url('https://fonts.googleapis.com/css2?family=Nunito+Sans:ital,wght@0,400;0,700;1,400;1,700&display=swap');
+
+body {
+    /* The image used */
+    background-image: url('/assets/background-colour.png');
+    background-attachment: fixed;
+    background-position: center;
+    background-repeat: repeat;
+    background-size: auto;
+}
+
 #app {
-    font-family: Avenir, Helvetica, Arial, sans-serif;
+    font-family: 'Nunito Sans', sans-serif;
     -webkit-font-smoothing: antialiased;
     -moz-osx-font-smoothing: grayscale;
     text-align: center;
     color: #2c3e50;
-}
-
-#nav {
-    padding: 30px;
-
-    a {
-        font-weight: bold;
-        color: #2c3e50;
-
-        &.router-link-exact-active {
-            color: #42b983;
-        }
-    }
 }
 </style>
