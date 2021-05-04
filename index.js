f41e5a6db1a0dce5b89bbed285a3442b354b1b1a
Alexander Bailey <alexkingstonbailey@gmail.com>
2021-05-04 18:01:01 +1200

    refactored typeDef import

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
 import { ApolloServer } from 'apollo-server'
-import { readFileSync } from 'fs'
+import { readdirSync, readFileSync } from 'fs'
 import resolvers from './resolvers'
 
-const queries = readFileSync('./schemas/schema.graphql').toString('utf-8')
-// const mutations = readFileSync('./schemas/mutations.graphql').toString('utf-8')
-const objects = readFileSync('./schemas/objects.graphql').toString('utf-8')
-// const inputs = readFileSync('./schemas/inputs.graphql').toString('utf-8')
+const schemaFiles = readdirSync('./schemas/').filter((file) =>
+    file.endsWith('.graphql'),
+)
+const typeDefs = schemaFiles.map((path) => {
+    return readFileSync('./schemas/' + path).toString('utf-8')
+})
 
 const server = new ApolloServer({
     resolvers,
-    typeDefs: [queries, objects],
+    typeDefs,
     introspection: true,
     playground: true,
     debug: true,
