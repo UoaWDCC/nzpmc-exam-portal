2ccd1c059f6753304773e5e3068cb65528554d1a
Alexander Bailey <alexkingstonbailey@gmail.com>
2021-05-10 15:28:55 +1200

    created datetime scalar

---
+++ scalars/datetime.js
@@ -0,0 +1,20 @@
+const { GraphQLScalarType, Kind } = require('graphql')
+
+const datetimeScalar = new GraphQLScalarType({
+    name: 'DateTime',
+    description: 'DateTime custom scalar type',
+    serialize(value) {
+        return value.getTime() // Convert outgoing Date to integer for JSON
+    },
+    parseValue(value) {
+        return new Date(value) // Convert incoming integer to Date
+    },
+    parseLiteral(ast) {
+        if (ast.kind === Kind.INT) {
+            return new Date(parseInt(ast.value, 10)) // Convert hard-coded AST string to integer and then to Date
+        }
+        return null // Invalid hard-coded value (not an integer)
+    },
+})
+
+export default datetimeScalar
