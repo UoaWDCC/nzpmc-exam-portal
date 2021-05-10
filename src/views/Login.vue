33a32ae7be0553b93e2e7f01e62171c58c2774a6
Lance Delos Reyes <58032488+lanxdrey19@users.noreply.github.com>
2021-05-10 14:56:08 +1200

    Nzpmc 003/implement login page (#1)
    * worked on css styling for login page
    
    * fixed css styling to center components more
    
    * worked on css styling for login page
    
    * fixed mistakes from review
    
    * fixed linting error
    
    * fixed formating
    
    * deleted redundant files

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,6 +1,48 @@
 <template>
     <div class="login">
-        <img src="/assets/logo.png" />
-        <h1>This is a login page</h1>
+        <v-container style="background-color: #ecf0f1; max-width: 500px">
+            <v-row align="center">
+                <v-col sm="12" align="center" justify="center">
+                    <img
+                        style="max-width: 100%"
+                        alt="NZPMC Logo"
+                        src="/assets/logo.png"
+                    />
+                </v-col>
+            </v-row>
+            <v-row dense>
+                <v-col>
+                    <v-text-field
+                        placeholder="Username"
+                        prepend-icon="mdi-account-circle"
+                        color="#03a9f4"
+                    />
+                </v-col>
+            </v-row>
+            <v-row dense>
+                <v-col>
+                    <v-text-field
+                        type="Password"
+                        placeholder="Password"
+                        prepend-icon="mdi-lock"
+                        append-icon="mdi-eye-off"
+                        color="#03a9f4"
+                    />
+                </v-col>
+            </v-row>
+            <v-row align="center" justify="center">
+                <v-col sm="12" align="end" justify="center">
+                    <v-btn x-large color="#03a9f4" style="max-width: 100%"
+                        >Login</v-btn
+                    >
+                </v-col>
+            </v-row>
+        </v-container>
     </div>
 </template>
+
+<style scoped>
+.login {
+    margin-top: 5rem;
+}
+</style>
