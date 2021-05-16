2952245f8df48fbc8e7db1a59721421e68083193
Alexander Bailey <alexkingstonbailey@gmail.com>
2021-05-16 15:30:09 +1200

    added resolver files

---
+++ resolvers/question.js
@@ -0,0 +1,35 @@
+const studQuestion = {
+    id: '123',
+    question: 'What is the question?',
+    noAnswers: 42,
+    topics: 'Physics, Existentialism',
+    options: [
+        {
+            id: '123',
+            option: 'Option1',
+            created: new Date(),
+            modified: new Date(),
+        },
+        {
+            id: '124',
+            option: 'Option2',
+            created: new Date(),
+            modified: new Date(),
+        },
+    ],
+    created: new Date(),
+    modified: new Date(),
+}
+
+const resolvers = {
+    Query: {
+        question(parents, args, ctx) {
+            return studQuestion
+        },
+        questions(parents, args, ctx) {
+            return [studQuestion, studQuestion, studQuestion]
+        },
+    },
+}
+
+export default resolvers
