4f0ba69894f22b72d7b61855f5faa5b17bab7cb4
Alexander Bailey <alexkingstonbailey@gmail.com>
2021-05-16 14:13:13 +1200

    studded user and users

--- resolvers/index.js
+++ resolvers/index.js
@@ -1,5 +1,6 @@
 import { merge } from 'lodash'
+import userResolver from './user'
 
 const rootResolver = {}
 
-export default merge(rootResolver)
+export default merge(rootResolver, userResolver)
--- resolvers/user.js
+++ resolvers/user.js
@@ -1,20 +1,28 @@
+const studUser = {
+    id: '123',
+    displayName: 'user',
+    email: 'user@email.com',
+    emailVerified: 'user@email.com',
+    photoURL: 'https://i.imgur.com/G4cy8en.jpg',
+    firstName: 'Joe',
+    lastName: 'Smith',
+    yearLevel: 9,
+    role: 'student',
+    created: Date.now(),
+    modified: Date.now(),
+}
+
 const resolvers = {
     Query: {
-        me(parents, args, ctx) {
-            return {
-                id: '123',
-                displayName: 'user',
-                email: 'user@email.com',
-                emailVerified: 'user@email.com',
-                photoURL: 'https://i.imgur.com/G4cy8en.jpg',
-                firstName: 'Joe',
-                lastName: 'Smith',
-                yearLevel: 9,
-                role: 'student',
-                created: Date.now(),
-                modified: Date.now(),
-            }
+        user(parents, args, ctx) {
+            return studUser
         },
+        me(parents, args, ctx) {
+            return studUser
+        },
+        users(parents, args, ctx) {
+            return [studUser, studUser, studUser, studUser]
+        } 
     },
 }
 
