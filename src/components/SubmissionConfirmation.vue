769a4d5444592a504123e66a93da7e15c030cdc7
Daniel Torrey <79776790+dtor068@users.noreply.github.com>
2021-05-30 14:32:19 +1200

    Nzpmc 021/implement submission confirmation popup (#15)
    * Started implementation of submission confirmation popup
    
    * Temporarily added SubmissionConfirmation component to Exam.vue in order to view changes.
    
    * Implemented submission confirmation popup component.
    
    * Deleted and reinstalled npm
    
    * Removed "i" form package.json

---
+++ src/components/SubmissionConfirmation.vue
@@ -0,0 +1,32 @@
+<template>
+    <div align="center">
+        <v-container style="background-color: #ecf0f1; max-width: 400px">
+            <v-container style="background-color: #c1c1c1; max-width: 400px">
+                <h3><b>Are you sure you want to submit?</b></h3>
+            </v-container>
+            <p style="padding: 15px 0px">
+                You will not be able to redo any questions after you submit.
+            </p>
+            <v-btn
+                style="
+                    margin-right: 10px;
+                    color: white;
+                    background-color: #09b683;
+                "
+                x-large
+            >
+                Yes
+            </v-btn>
+            <v-btn
+                style="
+                    margin-left: 10px;
+                    color: white;
+                    background-color: #ff7069;
+                "
+                x-large
+            >
+                No
+            </v-btn>
+        </v-container>
+    </div>
+</template>
