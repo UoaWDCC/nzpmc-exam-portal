19bbcf939a6c4d50842264b0bc63960da4384678
Will Zhang <yzhb120@aucklanduni.ac.nz>
2021-07-07 02:55:21 +1200

    fixed merge conflict

--- src/components/SingleAnswer.vue
+++ src/components/SingleAnswer.vue
@@ -2,17 +2,7 @@
     <v-card
         dark
         @click="selectanswer"
-<<<<<<< HEAD
         :color="this.selectedID === this.optionID ? '#03a9f5' : '#385F73'"
-    >
-        <div class="d-flex">
-            <latex class="font-weight-bold flex-grow-1 pa-4" :content="text" />
-            <span
-                v-if="this.selectedID === this.optionID"
-                class="material-icons ma-4"
-            >
-=======
-        :color="selectedID === optionID ? '#03a9f5' : '#385F73'"
         style="height: 100%"
     >
         <div class="d-flex" style="height: 100%; align-items: center">
@@ -22,7 +12,6 @@
                 style="width: calc(100% - 3.5rem); display: block"
             />
             <span v-if="selectedID === optionID" class="material-icons ma-4">
->>>>>>> master
                 check_circle
             </span>
         </div>
