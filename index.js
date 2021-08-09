2b0c541c94411d3587448c347b63ad8598f3feeb
ZHENK <chengzhenyang@gmail.com>
2021-08-09 15:40:53 +1200

    Update dependencies and Apollo server v3

--- index.js
+++ index.js
@@ -13,27 +13,37 @@ const typeDefs = schemaFiles.map((path) => {
     return readFileSync('./schemas/' + path).toString('utf-8')
 })
 
-const app = express()
-
-app.use(cors())
-
-const server = new ApolloServer({
-    resolvers,
-    typeDefs,
-    introspection: true,
-    playground: true,
-    debug: true,
-    context: auth(),
-})
-
-server.applyMiddleware({
-    app,
-})
-
 const port = process.env.PORT | 8080
-app.listen(port, () => {
-    console.log(`Server is ready at http://localhost:${port}`)
-    console.log(
-        `GraphQL Server is ready at http://localhost:${port}${server.graphqlPath}`,
-    )
-})
+
+const startApolloServer = async () => {
+    const server = new ApolloServer({
+        resolvers,
+        typeDefs,
+        introspection: true,
+        playground: true,
+        debug: true,
+        context: auth(),
+    })
+
+    await server.start()
+
+    const app = express()
+
+    app.use(cors())
+
+    // Mount Apollo middleware here.
+    server.applyMiddleware({
+        app,
+    })
+
+    app.listen(port, () => {
+        console.log(`Server is ready at http://localhost:${port}`)
+        console.log(
+            `GraphQL Server is ready at http://localhost:${port}${server.graphqlPath}`,
+        )
+    })
+
+    return { server, app }
+}
+
+startApolloServer()
