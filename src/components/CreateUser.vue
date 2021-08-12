dd118a551118512eb9b0773fa36e74c23e0648bc
dtor068 <dtor068@aucklanduni.ac.nz>
2021-08-12 15:24:14 +1200

    Tweaking on component

--- src/components/CreateUser.vue
+++ src/components/CreateUser.vue
@@ -1,73 +1,84 @@
 <template>
     <v-dialog v-model="dialog" width="500">
         <template v-slot:activator="{ on, attrs }">
-            <v-btn class="mx-2" fab v-bind="attrs" v-on="on">
-                <v-icon> mdi-plus </v-icon>
-            </v-btn>
+            <div class="text-center">
+                <v-btn class="mx-2" fab light v-bind="attrs" v-on="on">
+                    <v-icon dark> mdi-plus </v-icon>
+                </v-btn>
+            </div>
         </template>
-        <v-container style="height: 100%">
-            <v-card elevation="2" class="pa-6">
-                <h1 class="mb-2">
-                    {{ createUserMode ? 'Create User' : 'Create User' }}
-                </h1>
-                <v-form ref="userForm" v-model="formIsValid">
-                    <v-row dense>
-                        <v-col class="col">
-                            <v-text-field
-                                label="Name"
-                                :rules="[rules.required]"
-                            ></v-text-field>
-                        </v-col>
-                    </v-row>
-                    <v-row dense>
-                        <v-col>
-                            <v-text-field
-                                label="Email"
-                                :rules="[rules.required]"
-                            ></v-text-field>
-                        </v-col>
-                    </v-row>
-                    <v-row dense>
-                        <v-col>
-                            <v-text-field
-                                label="Year Level"
-                                type="number"
-                                :rules="[rules.required]"
-                            ></v-text-field>
-                        </v-col>
-                    </v-row>
-                    <v-row dense>
-                        <v-col>
-                            <v-text-field
-                                label="Password (optional)"
-                            ></v-text-field>
-                        </v-col>
-                    </v-row>
-                    <v-row dense>
-                        <v-col>
-                            <v-select
-                                :items="quizzes"
-                                label="Associated Quiz (optional)"
-                            ></v-select>
-                        </v-col>
-                    </v-row>
-                    <v-row>
-                        <v-col class="col-12 d-flex justify-end">
-                            <v-btn
-                                type="submit"
-                                color="primary"
-                                :disabled="!formIsValid"
-                            >
-                                <v-icon left class="material-icons">
-                                    {{ createUserMode ? 'add' : 'create' }}
-                                </v-icon>
-                                {{ createUserMode ? 'Create quiz' : 'Create' }}
-                            </v-btn>
-                        </v-col>
-                    </v-row>
-                </v-form>
-            </v-card>
-        </v-container>
+        <v-card elevation="2" class="pa-6">
+            <h1 class="mb-2">Create User</h1>
+            <v-form ref="userForm" v-model="formIsValid">
+                <v-row dense>
+                    <v-col class="col">
+                        <v-text-field
+                            label="Name"
+                            v-model="userName"
+                            :rules="[rules.required]"
+                        ></v-text-field>
+                    </v-col>
+                </v-row>
+                <v-row dense>
+                    <v-col>
+                        <v-text-field
+                            label="Email"
+                            v-model="userEmail"
+                            :rules="[rules.required]"
+                        ></v-text-field>
+                    </v-col>
+                </v-row>
+                <v-row dense>
+                    <v-col>
+                        <v-text-field
+                            label="Year Level"
+                            type="number"
+                            v-model="userYearLevel"
+                            :rules="[rules.required]"
+                        ></v-text-field>
+                    </v-col>
+                </v-row>
+                <v-row dense>
+                    <v-col>
+                        <v-text-field
+                            label="Password (optional)"
+                            v-model="userPassword"
+                        ></v-text-field>
+                    </v-col>
+                </v-row>
+                <v-row dense>
+                    <v-col>
+                        <v-select
+                            :items="quizzes"
+                            v-model="associatedQuiz"
+                            label="Associated Quiz (optional)"
+                        ></v-select>
+                    </v-col>
+                </v-row>
+                <v-row>
+                    <v-col class="d-flex justify-space-around">
+                        <v-btn
+                            type="submit"
+                            color="primary"
+                            :disabled="!formIsValid"
+                        >
+                            <v-icon left class="material-icons">
+                                create
+                            </v-icon>
+                            Create
+                        </v-btn>
+
+                        <v-btn
+                            type="submit"
+                            color="error"
+                            @click="nullVariables"
+                        >
+                            Cancel
+                        </v-btn>
+                    </v-col>
+                </v-row>
+            </v-form>
+        </v-card>
     </v-dialog>
 </template>
 
@@ -82,17 +93,25 @@ export default {
     data() {
         return {
             quizzes: ['Foo', 'Bar', 'Fizz', 'Buzz'],
-            detailsForm: null,
+            userName: null,
+            userPassword: null,
+            userYearLevel: null,
+            userEmail: null,
+            associatedQuiz: null,
+            userForm: null,
             formIsValid: null,
             rules: {
                 required: (value) => !!value || 'Required.',
             },
-            associatedQuiz: null,
         }
     },
-    computed: {
-        createUserMode() {
-            return this.$route.name === 'UserAdminCreateUser'
+    methods: {
+        nullVariables() {
+            this.userName = null
+            this.userPassword = null
+            this.userYearLevel = null
+            this.userEmail = null
+            this.associatedQuiz = null
         },
     },
 }
