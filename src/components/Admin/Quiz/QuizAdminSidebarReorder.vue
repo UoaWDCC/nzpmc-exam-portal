0af59a9ddd890e896ab6e792a1bec6713faf406c
Matthew Taylor <matthewaptaylor@gmail.com>
2022-05-24 08:34:23 +1200

    🐛 Fixed ESLint bug

--- src/components/Admin/Quiz/QuizAdminSidebarReorder.vue
+++ src/components/Admin/Quiz/QuizAdminSidebarReorder.vue
@@ -54,7 +54,7 @@ export default {
                     variables: {
                         quizId: this.quizId,
                         oldId: this.currentId,
-                        newId: newId,
+                        newId,
                     },
                     refetchQueries: [
                         {
--- src/gql/mutations/questions.js
+++ src/gql/mutations/questions.js
@@ -1,6 +1,4 @@
 import gql from 'graphql-tag'
-import { QuestionFragment } from '../fragments/question'
-import { OptionFragment } from '../fragments/option'
 
 export const SwapQuestionMutation = gql`
     mutation SwapQuestion($quizId: ID!, $oldId: ID!, $newId: ID!) {
