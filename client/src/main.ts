4bddee11b4fe06f902e2ac602def98627fdd402d
bcho892 <bcho892@aucklanduni.ac.nz>
2023-04-24 22:43:35 +1200

    feat: handle auth and add skeleton loader import

--- client/src/main.ts
+++ client/src/main.ts
@@ -3,13 +3,18 @@ import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
-import { createProvider } from "./vue-apollo"
+// Auth
+import { useMainStore } from '@/stores/main'
+import { auth } from './firebase'
+import { onAuthStateChanged } from 'firebase/auth'
+import { createProvider, onLogin, onLogout } from './vue-apollo'
 
 // Vuetify
 import 'vuetify/styles'
 import { createVuetify } from 'vuetify'
 import * as components from 'vuetify/components'
 import * as directives from 'vuetify/directives'
+import { VSkeletonLoader } from 'vuetify/labs/VSkeletonLoader'
 
 const customTheme = {
   dark: false,
@@ -23,28 +28,42 @@ const customTheme = {
     error: '#B00020',
     info: '#2196F3',
     success: '#4CAF50',
-    warning: '#FB8C00',
+    warning: '#FB8C00'
   }
 }
 
-
 const vuetify = createVuetify({
-  components,
+  components: {
+    VSkeletonLoader,
+    components
+  },
   directives,
   theme: {
     defaultTheme: 'customTheme',
     themes: {
-      customTheme,
+      customTheme
     }
-  },
+  }
 })
+const apolloProvider = createProvider()
+// Track user state in store
+onAuthStateChanged(auth, async (user) => {
+  // Update Apollo auth status
+  if (user) {
+    onLogin(apolloProvider.defaultClient, await user.getIdToken(true))
+  } else onLogout(apolloProvider.defaultClient)
 
+  // Let app know user state finalised
+  const store = useMainStore()
+  store.user = user
+  store.userLoading = false
+})
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
 app.use(vuetify)
-app.use(createProvider())
+app.use(apolloProvider)
 
 // app.use(VueLaTeX2JS)
 //TODO:
