f1b0125b37cf7db8c94ce635bb4f53165c7126d9
bcho892 <bcho892@aucklanduni.ac.nz>
2023-04-30 17:15:47 +1200

    feat: migrate over old colour sheme

--- client/src/main.ts
+++ client/src/main.ts
@@ -19,16 +19,13 @@ import { VSkeletonLoader } from 'vuetify/labs/VSkeletonLoader'
 const customTheme = {
   dark: false,
   colors: {
-    background: '#FFFFFF',
-    surface: '#FFFFFF',
-    primary: '#6200EE',
-    'primary-darken-1': '#3700B3',
-    secondary: '#03DAC6',
-    'secondary-darken-1': '#018786',
-    error: '#B00020',
-    info: '#2196F3',
-    success: '#4CAF50',
-    warning: '#FB8C00'
+    primary: '#03a9f4',
+    secondary: '#ff9e01',
+    accent: '#3f51b5',
+    error: '#f44336',
+    warning: '#ffeb3b',
+    info: '#00bcd4',
+    success: '#4caf50'
   }
 }
 
