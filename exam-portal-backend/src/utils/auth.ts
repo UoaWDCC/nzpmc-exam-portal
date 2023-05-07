3ff5eaff42bef4b11e7d1fe46768b4acccbb906b
bcho892 <AKLbc70@gmail.com>
2023-05-07 18:28:32 +1200

    fix: got rid of fireorm exception by deleting old auth check function

--- exam-portal-backend/src/utils/auth.ts
+++ exam-portal-backend/src/utils/auth.ts
@@ -2,17 +2,8 @@ import { AdminAuthenticationError } from './errors'
 import { getUser } from '../controllers'
 import { firestore } from './firebase'
 
-// READ https://www.apollographql.com/docs/apollo-server/data/errors/
-const checkRole = async (userID: string) => {
-    const currentUser = await getUser(userID)
-    if (currentUser.role !== 'admin') {
-        throw new AdminAuthenticationError()
-    }
-}
-
 const isAdminInFirestore = async (userID: string) => {
-    return true
-    const userDoc = await firestore.collection('users').doc(userID).get()
+    const userDoc = await firestore.collection('Users').doc(userID).get()
     if (userDoc.exists) {
         const userData = userDoc.data()
         if (userData?.role === 'admin') {
