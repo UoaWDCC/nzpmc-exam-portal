8878d7f632d79685488f80f922b6709a8626cdb7
bcho892 <bcho892@aucklanduni.ac.nz>
2023-05-07 18:24:26 +1200

    refactor: check role using manual query

--- exam-portal-backend/src/utils/auth.ts
+++ exam-portal-backend/src/utils/auth.ts
@@ -1,5 +1,6 @@
 import { AdminAuthenticationError } from './errors'
 import { getUser } from '../controllers'
+import { firestore } from './firebase'
 
 // READ https://www.apollographql.com/docs/apollo-server/data/errors/
 const checkRole = async (userID: string) => {
@@ -10,10 +11,14 @@ const checkRole = async (userID: string) => {
 }
 
 const isAdminInFirestore = async (userID: string) => {
-    const currentUser = await getUser(userID)
-    if (currentUser.role === 'admin') {
-        return true
+    return true
+    const userDoc = await firestore.collection('users').doc(userID).get()
+    if (userDoc.exists) {
+        const userData = userDoc.data()
+        if (userData?.role === 'admin') {
+            return true
+        }
     }
     return false
 }
-export { checkRole, isAdminInFirestore }
+export { isAdminInFirestore }
