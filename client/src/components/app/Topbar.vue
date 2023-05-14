99f492cde14ec7d87592035a4242bea9c4b9cdfd
luorixo <luorixo@gmail.com>
2023-05-14 15:10:40 +1200

    added ts typing to LoadingScreen.vue, Snackbar.vue, Topbar.vue

--- client/src/components/app/Topbar.vue
+++ client/src/components/app/Topbar.vue
@@ -1,4 +1,4 @@
-<script setup>
+<script setup lang="ts">
 import iconSvg from '@/assets/icon.svg'
 </script>
 
@@ -33,7 +33,7 @@ import iconSvg from '@/assets/icon.svg'
     </v-app-bar>
 </template>
 
-<script>
+<script lang="ts">
 import { auth } from '@/firebase'
 import { signOut } from 'firebase/auth'
 import { mapWritableState } from 'pinia'
