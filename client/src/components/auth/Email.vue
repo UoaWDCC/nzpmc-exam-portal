7d8d136d014b99caa22f1fec6666792660cbd28b
Kevin Cheung <48199865+nzkevc@users.noreply.github.com>
2023-05-14 21:49:01 +1200

    refactor: use simpler regex in auth/Email.vue

--- client/src/components/auth/Email.vue
+++ client/src/components/auth/Email.vue
@@ -1,41 +1,17 @@
 <template>
-    <v-form
-        ref="form"
-        v-model="valid"
-        :disabled="loading"
-        class="auth-email"
-        @submit="nextPanel"
-    >
-        <AuthHeader
-            title="Authenticate"
-            text="Please sign in or sign up to continue."
-        />
+    <v-form ref="form" v-model="valid" :disabled="loading" class="auth-email" @submit="nextPanel">
+        <AuthHeader title="Authenticate" text="Please sign in or sign up to continue." />
 
         <div class="pb-4 px-4">
-            <v-text-field
-                v-model="currentEmail"
-                label="Email"
-                placeholder="john.smith@example.com"
-                type="email"
-                autocomplete="username"
-                hide-details="auto"
-                :rules="[
+            <v-text-field v-model="currentEmail" label="Email" placeholder="john.smith@example.com" type="email"
+                autocomplete="username" hide-details="auto" :rules="[
                     (v) => !!v || 'This is required',
                     (v) =>
                         emailRegex.test(v) ||
                         'Please enter a valid email address',
-                ]"
-                required
-                autofocus
-            ></v-text-field>
+                ]" required autofocus></v-text-field>
 
-            <input
-                type="password"
-                autocomplete="current-password"
-                label="Password"
-                style="height: 0"
-                tabindex="-1"
-            />
+            <input type="password" autocomplete="current-password" label="Password" style="height: 0" tabindex="-1" />
         </div>
 
         <v-expand-transition>
@@ -45,13 +21,7 @@
         </v-expand-transition>
 
         <div class="d-flex justify-end pb-4 px-4">
-            <v-btn
-                color="primary"
-                type="submit"
-                :disabled="!valid || loading"
-                :loading="loading"
-                text
-            >
+            <v-btn color="primary" type="submit" :disabled="!valid || loading" :loading="loading" text>
                 Continue
 
                 <v-icon right dark>mdi-arrow-right</v-icon>
@@ -85,8 +55,7 @@ export default {
             error: null,
 
             currentEmail: this.email,
-            emailRegex:
-                /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/,
+            emailRegex: /^[^\s@]+@[^\s@]+\.[^\s@]+$/,
         }
     },
 
