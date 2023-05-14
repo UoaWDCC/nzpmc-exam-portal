03feae2500e8a062900379c725168e79e45f23e3
Kevin Cheung <48199865+nzkevc@users.noreply.github.com>
2023-05-15 01:17:48 +1200

    refactor: add typing to app/exam/Question/Loader.vue

--- client/src/components/app/exam/Question/Loader.vue
+++ client/src/components/app/exam/Question/Loader.vue
@@ -1,55 +1,31 @@
 <template>
-    <div
-        style="overflow-y: hidden"
-        class="app-exam-question-loader flex-grow-1"
-    >
+    <div style="overflow-y: hidden" class="app-exam-question-loader flex-grow-1">
         <v-container fluid>
             <v-row>
                 <v-col cols="12" md="6" class="mb-n3">
                     <div class="align-center d-flex mb-3">
                         <div class="flex-grow-1">
-                            <v-skeleton-loader
-                                type="text"
-                                width="160"
-                                style="margin-top: 6px"
-                            />
+                            <v-skeleton-loader type="text" width="160" style="margin-top: 6px" />
                         </div>
 
-                        <v-skeleton-loader
-                            type="button"
-                            width="36"
-                            style="border-radius: 50%"
-                        />
+                        <v-skeleton-loader type="button" width="36" style="border-radius: 50%" />
                     </div>
 
-                    <v-skeleton-loader
-                        type="paragraph"
-                        style="margin-top: 18px; margin-bottom: -6px"
-                    />
+                    <v-skeleton-loader type="paragraph" style="margin-top: 18px; margin-bottom: -6px" />
                 </v-col>
 
                 <v-col cols="12" md="6">
-                    <v-skeleton-loader
-                        type="text"
-                        width="160"
-                        class="align-center d-flex"
-                        style="margin-top: 12px; margin-bottom: 18px"
-                    />
+                    <v-skeleton-loader type="text" width="160" class="align-center d-flex"
+                        style="margin-top: 12px; margin-bottom: 18px" />
 
-                    <v-skeleton-loader
-                        v-for="i in 3"
-                        :key="i"
-                        type="list-item"
-                        height="56"
-                        class="mb-3 option-skeleton"
-                    />
+                    <v-skeleton-loader v-for="i in 3" :key="i" type="list-item" height="56" class="mb-3 option-skeleton" />
                 </v-col>
             </v-row>
         </v-container>
     </div>
 </template>
 
-<script>
+<script lang="ts">
 import { TOOLBAR_HEIGHT } from '@/helpers'
 
 export default {
@@ -64,7 +40,7 @@ export default {
 </script>
 
 <style>
-.option-skeleton > * {
+.option-skeleton>* {
     height: 100%;
 }
 </style>
