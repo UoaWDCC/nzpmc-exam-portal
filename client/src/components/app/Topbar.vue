99daab817b21c4ac09352b9aa3af96edb28fe9ab
Kevin Cheung <48199865+nzkevc@users.noreply.github.com>
2023-05-15 01:27:37 +1200

    refactor: remove manual defineComponent from Topbar.vue because consistency

--- client/src/components/app/Topbar.vue
+++ client/src/components/app/Topbar.vue
@@ -5,17 +5,8 @@ import iconSvg from '@/assets/icon.svg'
 <template>
     <v-app-bar app color="primary" dark dense height="48px" class="app-topbar">
         <v-toolbar-title class="ma-n1 pa-1">
-            <router-link
-                :to="{ name: 'App' }"
-                class="align-center d-flex mr-2 text-decoration-none white--text"
-            >
-                <v-img
-                    :src="iconSvg"
-                    width="32px"
-                    aspect-ratio="1"
-                    contain
-                    class="flex-grow-0 mr-2"
-                />
+            <router-link :to="{ name: 'App' }" class="align-center d-flex mr-2 text-decoration-none white--text">
+                <v-img :src="iconSvg" width="32px" aspect-ratio="1" contain class="flex-grow-0 mr-2" />
 
                 <div class="text-truncate">NZPMC</div>
             </router-link>
@@ -27,7 +18,7 @@ import iconSvg from '@/assets/icon.svg'
             <v-btn :to="{ name: 'AppExams' }">My Exams</v-btn>
 
             <v-btn :to="{ name: 'AppAdmin' }">Admin</v-btn>
-			<v-btn @click="signOut(auth)">Log Out</v-btn>
+            <v-btn @click="signOut(auth)">Log Out</v-btn>
         </v-toolbar-items>
 
     </v-app-bar>
@@ -38,9 +29,8 @@ import { auth } from '@/firebase'
 import { signOut } from 'firebase/auth'
 import { mapWritableState } from 'pinia'
 import { useMainStore } from '@/stores/main'
-import { defineComponent } from 'vue'
 
-export default defineComponent({
+export default {
     name: 'AppTopbar',
 
     computed: {
@@ -56,5 +46,5 @@ export default defineComponent({
             })
         },
     },
-})
+}
 </script>
