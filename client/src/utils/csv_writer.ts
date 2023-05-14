b8555937ba6659fd49e00ad444b917a8e5aacef1
Aaron Rodrigues <ardo715@aucklanduni.ac.nz>
2023-05-15 03:12:27 +1200

    edit: Added todos and set up testing

--- client/src/utils/csv_writer.ts
+++ client/src/utils/csv_writer.ts
@@ -1,6 +1,28 @@
-import {createObjectCsvWriter } from '../../../node_modules/csv-writer';
+import {createObjectCsvWriter } from 'csv-writer';
+import type {Student} from './csv_parser';
+import * as path from 'path';
 
-// const csvWriter = createObjectCsvWriter({
-//   path: 'todo',
-//   header: ['todo'],
-// });
+/**
+* Generates a CSV file from the given students array
+* @param students - The array of students to generate the CSV file from
+* @returns A promise that resolves when the CSV file has been generated
+*/
+export async function generateCsvFile(students: Student[]): Promise<void> {
+    // TODO: Implement the generateCsvFile function
+
+    const csvWriter = createObjectCsvWriter({
+        path: "", //TODO: Implement the path (Where will the file be written to?)
+        header: [
+            //TODO: Implement the headers (16 total) e.g.
+            //id: 'firstName', title: 'First Name',
+        ]
+    });
+
+    try {
+        // Write the student records to the csv file
+        await csvWriter.writeRecords(students);
+    }
+    catch (err) {
+        console.log(err);
+    }
+}
--- client/src/utils/csv_writer_test_script.ts
+++ client/src/utils/csv_writer_test_script.ts
@@ -1,1 +1,21 @@
-//TODO import csv writer
+import {generateCsvFile } from './csv_writer';
+import {parseCSV} from './csv_parser';
+
+/**
+ * Main function
+ * @returns A promise that resolves when the CSV file has been generated
+ */
+async function main(): Promise<void> {
+    try {
+        // gets the students array from the csv_parser.ts
+        const students = await parseCSV('csv_dummy_template.csv');
+        
+        // Calls the generateCsvFile function from csv_writer.ts
+        await generateCsvFile(students);
+    } 
+    catch (error) {
+        console.error('An error occurred:', error);
+    }
+  }
+
+main();
