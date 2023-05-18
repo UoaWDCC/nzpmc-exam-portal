f2f3c24cd8fdda4b8faeed0a705d3563e1676cc3
bcho892 <bcho892@aucklanduni.ac.nz>
2023-05-18 16:49:31 +1200

    chore: add example implementation for checking admins status to admin page

--- client/src/components/app/admin/index.vue
+++ client/src/components/app/admin/index.vue
@@ -1,10 +1,23 @@
 <template>
-    <div class="app-admin">Admin</div>
+    <div class="app-admin">
+		<div v-if="store.userIsAdmin">
+			Admin
+		</div>
+		<div v-else>Not Admin</div>
+	</div>
 </template>
 
 <script>
+import {useMainStore} from '@/stores/main';
+
 export default {
     name: 'AppAdmin',
+
+	data() {
+		return {
+			store: useMainStore()
+		}
+	},
 
     metaInfo: {
         title: 'Admin',
