76ee1bdc6eea5e838e61afb20056517b566ec5e4
bcho892 <bcho892@aucklanduni.ac.nz>
2023-06-11 17:21:19 +1200

    feat: start working on mutation for adding users

--- client/src/components/app/admin/UserManagement.vue
+++ client/src/components/app/admin/UserManagement.vue
@@ -1,49 +1,47 @@
-
 <template>
-<v-file-input 
- prepend-icon=""
- ref="csvUpload"
- @change="handleCsvUpload"
-accept=".csv"
-label="Upload CSV"></v-file-input>
-<v-btn @click="addUsersWithCsv()">Add users using CSV</v-btn>
-<v-btn @click="deleteUsers()">Delete users with emails</v-btn>
-<v-btn @click="downloadUsersCsv()">Download CSV of users</v-btn>
+  <v-file-input
+    prepend-icon=""
+    ref="csvUpload"
+    @change="handleCsvUpload"
+    accept=".csv"
+    label="Upload CSV"
+  ></v-file-input>
+  <v-btn @click="addUsersWithCsv()">Add users using CSV</v-btn>
+  <v-btn @click="deleteUsers()">Delete users with emails</v-btn>
+  <v-btn @click="downloadUsersCsv()">Download CSV of users</v-btn>
 </template>
 
 <script lang="ts">
-import {deleteUsers, addUsersWithCsv, downloadUsersCsv} from '../../../utils/userManagement';
+import { deleteUsers, addUsersWithCsv, downloadUsersCsv } from '../../../utils/userManagement'
 
 export default {
-    name: 'UserManagement',
+  name: 'UserManagement',
 
-    metaInfo: {
+  metaInfo: {},
+
+  components: {},
+
+  data() {
+    return {
+      currentCsv: File
+    }
+  },
+
+  methods: {
+    handleCsvUpload(event: Event) {
+      const input = event.target as FileInput
+      const file = input.files?.[0]
+
+      if (file) {
+        console.log('uploaded')
+        this.currentCsv = file
+      } else {
+        this.currentCsv = undefined
+      }
     },
-
-    components: {  },
-
-
-    data() {
-        return {
-			currentCsv: File,
-        }
-    },
-
-    methods: {
-		handleCsvUpload(event: Event){
-			const input = event.target as FileInput;
-			const file = input.files?.[0];
-
-			if(file){
-				console.log("uploaded")
-				this.currentCsv = file;
-			} else {
-				this.currentCsv = undefined;
-			}
-		},
-		addUsersWithCsv,
-		deleteUsers,
-		downloadUsersCsv
-    },
+    addUsersWithCsv,
+    deleteUsers,
+    downloadUsersCsv
+  }
 }
 </script>
