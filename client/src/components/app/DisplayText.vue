e75de9e9f035eb8bd3bc9c4262636e23e5d76d66
bcho892 <bcho892@aucklanduni.ac.nz>
2023-07-29 22:35:14 +1200

    fix: remove require syntax from displaytext

--- client/src/components/app/DisplayText.vue
+++ client/src/components/app/DisplayText.vue
@@ -1,35 +1,35 @@
 <template>
-    <latex :content="parsed" style="margin-bottom: -16px" />
+  <latex :content="parsed" style="margin-bottom: -16px" />
 </template>
 
 <script lang="ts">
-var showdown = require('showdown')
+import showdown from 'showdown'
 
 export default {
-    name: 'AppDisplayText',
+  name: 'AppDisplayText',
 
-    props: {
-        text: { type: String, required: true },
-    },
+  props: {
+    text: { type: String, required: true }
+  },
 
-    data() {
-        return {
-            converter: new showdown.Converter(),
-        }
-    },
+  data() {
+    return {
+      converter: new showdown.Converter()
+    }
+  },
 
-    computed: {
-        parsed() {
-            return this.converter.makeHtml(this.text)
-        },
-    },
+  computed: {
+    parsed() {
+      return this.converter.makeHtml(this.text)
+    }
+  }
 }
 </script>
 
 <style>
 img {
-    max-width: min(100%, 300px);
-    margin: 0 auto;
-    display: block;
+  max-width: min(100%, 300px);
+  margin: 0 auto;
+  display: block;
 }
 </style>
