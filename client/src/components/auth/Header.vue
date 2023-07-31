09d36147b77c104731d366d0935d49a3b7c31415
alexmingikim <alex.kim9905@gmail.com>
2023-07-31 13:50:04 +1200

    style: change back button

--- client/src/components/auth/Header.vue
+++ client/src/components/auth/Header.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="align-center auth-header d-flex header">
-        <v-btn v-if="showBack" icon color="secondary" class="ml-0" large @click="$emit('back')">
-            <v-icon color="white">mdi-arrow-left</v-icon>
+        <v-btn v-if="showBack" icon flat @click="$emit('back')">
+            <v-icon color="secondary" size="40">mdi-chevron-left</v-icon>
         </v-btn>
 
         <div class="header__text">
