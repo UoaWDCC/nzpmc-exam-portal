18242a2fafce9a8fe3d13b4aaf3ec72eb1aee034
bcho892 <bcho892@aucklanduni.ac.nz>
2023-08-19 17:57:11 +1200

    feat: :poop: style overlay when submitting

--- client/src/components/app/exam/SubmittingOverlay.vue
+++ client/src/components/app/exam/SubmittingOverlay.vue
@@ -1,23 +1,50 @@
 <template>
     <v-dialog persistent v-model="examStore.submitting">
-        <v-card width="50%">love</v-card>
+        <v-card class="overlay-card">
+            <div class="logo-container mb-6">
+                <v-img contain id="logo" :src="logo" />
+            </div>
+            <h2>We are submitting your exam.</h2>
+            <p>Please wait...</p>
+            <v-progress-circular class="mt-4" color="secondary" :size="50" indeterminate></v-progress-circular>
+        </v-card>
     </v-dialog>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
 import { useExamStore } from './examStore'
+import topbar_nzpmc_logo from '@/assets/topbar_nzpmc_logo.png'
 
 export default defineComponent({
     name: 'AppSubmittingOverlay',
 
     data() {
         return {
-            examStore: useExamStore()
+            examStore: useExamStore(),
+            logo: topbar_nzpmc_logo
         }
     }
 
 })
 </script>
 
-<style scoped></style>
+<style scoped lang="scss">
+.overlay-card {
+    justify-content: center;
+    align-items: center;
+    color: $white;
+    background-color: $primary;
+    width: 40rem;
+    height: 20rem;
+    align-self: center;
+
+    .logo-container {
+
+        #logo {
+            width: 30rem;
+        }
+    }
+}
+</style>
+
