c2f16d65d8f56ba69c21e5fdf9659d365ef47920
bcho892 <bcho892@aucklanduni.ac.nz>
2023-09-04 18:51:58 +1200

    fix: :poop: use correct userid when enrolling user in quiz

--- client/src/utils/quizManagement.ts
+++ client/src/utils/quizManagement.ts
@@ -88,7 +88,7 @@ export const enrolUsersInQuizFromCSV = async (
   try {
     const students = await parseCSVPapaparse(csv)
     console.log(students)
-    const studentEmails = students.map((student) => ({ id: ``, email: student.email }))
+    const studentEmails = students.map((student) => ({ id: null, email: student.email }))
     const mutation = await apollo.mutate({
       mutation: EnrolUsersInQuizMutation,
       variables: {
