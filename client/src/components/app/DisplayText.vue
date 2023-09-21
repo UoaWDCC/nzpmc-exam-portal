f79f56856abac813d91f65ec30d7765eea1a4107
johnchen383 <john.bm.chen@gmail.com>
2023-09-21 15:25:00 +1200

    feat: latex is working wooooo :)))

--- client/src/components/app/DisplayText.vue
+++ client/src/components/app/DisplayText.vue
@@ -1,26 +1,66 @@
 <template>
-  <div v-html="parsed"></div>
+  <div v-html="htmlContent" class="question-form"></div>
 </template>
 
 <script lang="ts">
 import { Converter } from 'showdown'
+import 'katex/dist/katex.min.css'
+import katex from 'katex'
 
 export default {
   name: 'AppDisplayText',
-
   props: {
     text: { type: String, required: true }
   },
 
   data() {
     return {
-      converter: new Converter()
+      converter: new Converter(),
+      htmlContent: '<strong>This is bold text.</strong>'
     }
   },
 
-  computed: {
+  mounted() {
+    this.parsed()
+  },
+
+  methods: {
     parsed() {
-      return this.converter.makeHtml(this.text)
+      this.htmlContent = ""
+      const regex = /\$(\$?)(.*?)\1\$/g
+
+      const latexStrings = []
+      let match
+      while ((match = regex.exec(this.text.replace(/\$\$/gi, '$'))) !== null) {
+        latexStrings.push('$' + match[2] + '$')
+      }
+
+      let html = ''
+      let workingString = this.text.replace(/\$\$/gi, '$')
+
+      latexStrings.forEach((latexString) => {
+        const parts = workingString.split(latexString)
+        html += parts[0] //add non latex part
+
+        //add the latex part
+        html += `<div class="latex">${latexString}</div>`
+
+        workingString = parts[1]
+      })
+
+      html += workingString
+
+      this.htmlContent = this.converter.makeHtml(html)
+
+      this.$nextTick(() => {
+        document.querySelectorAll('.latex').forEach((node) => {
+          let latex = node.textContent
+          let convertedLatex = latex?.slice(1, -1);
+          katex.render(convertedLatex, node, {
+            throwOnError: false
+          })
+        })
+      });
     }
   }
 }
