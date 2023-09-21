0d68273d7155e4fc6943d14143dffe20431f8818
Lint Action <lint-action@samuelmeuli.com>
2023-09-21 04:05:00 +0000

    Fix code style issues with Prettier

--- client/src/components/app/DisplayText.vue
+++ client/src/components/app/DisplayText.vue
@@ -32,7 +32,7 @@ export default {
 
       const latexStrings = []
       const imageSubstrings = []
-      let match;
+      let match
       while ((match = latexRegex.exec(this.text.replace(/\$\$/gi, '$'))) !== null) {
         latexStrings.push('$' + match[2] + '$')
       }
@@ -42,7 +42,7 @@ export default {
       }
 
       let html = '<div>'
-      let workingString = this.text.replace(/\$\$/gi, '$').replace(imageRegex, "");
+      let workingString = this.text.replace(/\$\$/gi, '$').replace(imageRegex, '')
 
       latexStrings.forEach((latexString) => {
         const parts = workingString.split(latexString)
@@ -55,8 +55,8 @@ export default {
       })
 
       html += `<p>${workingString}</p></div>`
-      imageSubstrings.forEach(img => {
-        html += img;
+      imageSubstrings.forEach((img) => {
+        html += img
       })
 
       this.htmlContent = this.converter.makeHtml(html)
