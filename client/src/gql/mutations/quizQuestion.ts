d6cc81dae7e8b9d1eaf13354e4015763aff59f7e
bcho892 <bcho892@aucklanduni.ac.nz>
2023-09-25 11:12:21 +1300

    feat: :sparkles: define function for editing question

---
+++ client/src/gql/mutations/quizQuestion.ts
@@ -0,0 +1,9 @@
+import gql from 'graphql-tag'
+
+export const EditQuestionMutation = gql`
+  mutation EditQuestion($input: EditQuestionInput!) {
+    editQuestion(input: $input) {
+      id
+    }
+  }
+`
--- client/src/utils/quizEditingMixin.ts
+++ client/src/utils/quizEditingMixin.ts
@@ -2,6 +2,8 @@ import { GetQuizInfoQuery } from '@/gql/queries/quiz'
 import { UserQuizQuery } from '@/gql/queries/userQuiz'
 import { useMainStore } from '@/stores/main'
 import { useRoute } from 'vue-router'
+import type { ApolloClient, NormalizedCacheObject } from '@apollo/client'
+import { EditQuestionMutation } from '@/gql/mutations/quizQuestion'
 
 export default {
   computed: {
@@ -18,12 +20,34 @@ export default {
     }
   },
   methods: {
-    async editQuiz(inputs: {
-      questionID: string
-      quizID: string
-      topics?: string
-      imageURI?: string
-      questionDescription?: string
-    }) {}
+    async editQuiz(
+      apollo: ApolloClient<NormalizedCacheObject>,
+      inputs: {
+        questionID: string
+        quizID: string
+        topics?: string
+        imageURI?: string
+        questionDescription?: string
+      }
+    ) {
+      const { questionID, quizID, topics, imageURI, questionDescription } = inputs
+      try {
+        const mutation = await apollo.mutate({
+          mutation: EditQuestionMutation,
+          variables: {
+            id: questionID,
+            quizID,
+            topics,
+            imageURI,
+            question: questionDescription
+          }
+        })
+        if (mutation.data) return true
+        else return false
+      } catch (e) {
+        console.error(e)
+        return false
+      }
+    }
   }
 }
