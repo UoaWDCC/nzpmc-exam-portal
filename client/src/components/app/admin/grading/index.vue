0c7b99b1db693d2535ffaaa900d722670383e19b
bcho892 <bcho892@aucklanduni.ac.nz>
2023-10-08 13:21:14 +1300

    feat: :sparkles: create basic container for grading page

--- client/src/components/app/admin/grading/index.vue
+++ client/src/components/app/admin/grading/index.vue
@@ -1,5 +1,16 @@
-<style></style>
-<template></template>
+<style>
+.app-admin-grading {
+  padding-top: 64px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 31px 0;
+  margin: 0;
+}
+</style>
+<template>
+  <v-container class="app-admin-grading" fluid></v-container>
+</template>
 <script lang="ts">
 import { defineComponent } from 'vue'
 export default defineComponent({
