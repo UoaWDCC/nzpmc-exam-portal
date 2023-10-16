231aa9f2caf55e3654d5dcc94cb0eb24b5c83897
bcho892 <AKLbc70@gmail.com>
2023-10-17 12:35:02 +1300

    feat: :construction: attempt to submit quiz on query of quiz

--- service/src/controllers/userQuiz.ts
+++ service/src/controllers/userQuiz.ts
@@ -25,6 +25,18 @@ const getUserQuiz = async (userQuizID: string): Promise<UserQuizModel> => {
 
         const expired =
             (userQuiz.closeTime && userQuiz.closeTime < new Date()) ?? true
+
+        // Submit quiz if already started
+        if (userQuiz.quizStart) {
+            const currentTimeSeconds = Math.floor(Date.now() / 1000)
+            const elapsedSeconds = currentTimeSeconds - userQuiz.quizStart
+            const secondsRemaining = quiz.duration.valueOf() * 60 - elapsedSeconds
+
+            if (secondsRemaining <= 0) {
+                userQuiz.submitted = true
+                await UserQuizTranRepository.update(userQuiz)
+            }
+        }
         return packUserQuiz({
             userID: userQuiz.userID,
             quiz,
@@ -211,7 +223,7 @@ const addUserQuiz = async (
             throw new NotFoundError()
         }
 
-        ;(await questions.find()).map((question) => {
+        ; (await questions.find()).map((question) => {
             addUserQuizQuestion(userQuiz.id, question.id)
         })
     })
